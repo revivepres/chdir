cbe5a1f2b71bab08fc4fafbf23679b043be15dc4
John Lee <johnlee@solidfish.com>
2018-09-23 23:24:46 -0700

    Removed API call for page load and using google.script instead

--- Code.gs.js
+++ Code.gs.js
@@ -1,38 +1,48 @@
 function doGet(request) {
-  Logger.log("Request Recieved: " + request);
-  if (request.parameter && request.parameter.list) {
-    var ss = SpreadsheetApp.openById("1y9twuw3jdZtZhY_bu83EGWwp-mwtzM4cN_wY3RagKNo");
-    var sheet = ss.getSheetByName("Roster");
-    var range = sheet.getDataRange();
-    var values = range.getValues();
-    var headers = values.shift().map(function (header) { return normalizeName(header); })
-
-    // Ignore blank rows
-    var filtered = values.filter(function (row) {
-      if (row[3] && row[4]) { return true; }
-      return false;
-    });
-
-    // Map rows to JSON
-    var data = filtered.map(function (row) {
-      var reducedRow = row.reduce(function (accum, value, index, arr) {
-        accum[headers[index]] = value;
-        if (index >= 4) {
-          arr.splice(1); // break iteration
-        }
-        return accum;
-      }, {});
-      return reducedRow;
-    });
-
-    return ContentService.createTextOutput(JSON.stringify(data)).setMimeType(ContentService.MimeType.JSON);
-  } else {
-    return HtmlService.createTemplateFromFile("index").evaluate().setTitle("RPC - Attendance");
-  }
+  var html = HtmlService.createTemplateFromFile("index").evaluate().setTitle("RPC - Attendance");
+  html.addMetaTag('viewport', 'width=device-width, initial-scale=1');
+  return html;
 }
 
 function include(filename) {
   return HtmlService.createTemplateFromFile(filename).getRawContent();
+}
+
+function load() {
+  var ss = SpreadsheetApp.openById("spreadsheetid");
+  var sheet = ss.getSheetByName("Roster");
+  var range = sheet.getDataRange();
+  var values = range.getValues();
+  var headers = values.shift().map(function (header) { return normalizeName(header); })
+
+  // Ignore blank rows and non active users
+  var filtered = values.filter(function (row) {
+    if (row[3] && row[4]) { 
+      if (row[6] && row[6].toLowerCase().indexOf("y") > -1) {
+        return true; 
+      }
+    }
+    return false;
+  });
+
+  // Map rows to JSON
+  var data = filtered.map(function (row) {
+    var reducedRow = row.reduce(function (accum, value, index, arr) {
+      accum[headers[index]] = value;
+      if (index >= 4) {
+        arr.splice(1); // break iteration
+      }
+      return accum;
+    }, {});
+    return reducedRow;
+  });
+
+  // Sort the result
+  data.sort(function (a, b) {
+    return a.firstname > b.firstname ? 1 : a.firstname < b.firstname ? -1 : 0;
+  });
+  
+  return data;
 }
 
 function save(data) {
@@ -43,7 +53,7 @@ function save(data) {
     var total = data.total;
     var members = data.members;
 
-    var ss = SpreadsheetApp.openById("1sWZD309C7cPL7zoESgmj5dUTdsRoF0dpMVh5eKkw12M");
+    var ss = SpreadsheetApp.openById("spreadsheetid");
     var sheet = ss.getSheetByName("Weekly");
     var range = sheet.getRange("A:A");
     var values = range.getValues();
--- app.js
+++ app.js
@@ -13,9 +13,6 @@ $(document).ready(function () {
   new MDCRipple(document.querySelector('.btnGuestdec'));
   new MDCRipple(document.querySelector('.btnGuestinc'));
   new MDCTextField(document.querySelector('.selectedDate'));
-
-  // Load the data
-  loadData();
 });
 
 // EVENT HANDLERS
@@ -38,26 +35,17 @@ $("#btnGuestdec").click(function (event) {
   GuestCount.minus();
 });
 
-// $("#btnSave").click(function (event) {
-//   // Get selected users on list
-//   var selected = [];
-//   $(".mdc-checkbox input:checked").each(function () {
-//     var id = $(this).attr("id");//.slice(3);
-//     var name = $(this).closest('li').find('label').text();
-//     selected.push({ id: id, name: name });
-//   });
-
-//   // Setup the object to save
-//   var total = GuestCount.get() + selected.length;
-//   var attendance = {
-//     week: SelectedDate.get(),
-//     guests: GuestCount.get(),
-//     total: total,
-//     members: selected
-//   };
-//   saveData(attendance);
-// });
+// EXPORTED METHODS
 // -----------------------------------------------
+var GetSelected = (function () {
+  var selected = [];
+  $(".mdc-checkbox input:checked").each(function () {
+    var id = $(this).attr("id");//.slice(3);
+    var name = $(this).closest('li').find('label').text();
+    selected.push({ id: id, name: name });
+  });
+  return selected;
+});
 
 var GuestCount = (function () {
   var count = 0;
@@ -111,127 +99,96 @@ var SelectedDate = (function () {
   }
 })();
 
-function loadData() {
-  drawPlaceholder();
-  $.ajax({
-    url: 'https://script.google.com/macros/s/URL/exec?list=true',
-    type: "get",
-    dataType: "json",
-    success: function (data, textStatus, jqXHR) {
-      // sort then draw
-      data.sort(function (a, b) {
-        //return a.firstname > b.firstname ? 1 : a.firstname < b.firstname ? -1 : 0;
-        return a.lastname > b.lastname ? 1 : a.lastname < b.lastname ? -1 : 0;
-      });
-      drawData(data);
+var List = (function () {
+  function draw(data) {
+    // Check if data state already stored
+    var store = localStorage.getItem("chdir");
+    var members = [];
+    if (store) {
+      var storedata = JSON.parse(store);
+
+      // Only load stored data if from today
+      if (SelectedDate.today() == storedata.week) {
+        GuestCount.init(storedata.guests);
+        if (storedata.members) {
+          members = storedata.members.map(function (member) {
+            return member.id;
+          });
+        }
+      }
+    }
+
+    $.each(data, function (index, item) {
+      var checked = members.indexOf(item.pid.toString()) > -1 ? "checked" : "";
+      var person = $(`
+         <li class="mdc-list-item checkbox-list-ripple-surface">
+         <span class="mdc-list-item__graphic" role="presentation">
+           <i class="material-icons">how_to_reg</i>
+         </span>
+         <span class="mdc-list-item__text">
+           <label for="${item.pid}">${item.firstname} ${item.lastname}</label>
+         </span>
+         <span class="mdc-list-item__meta">
+           <div class="mdc-checkbox">
+             <input type="checkbox" class="mdc-checkbox__native-control" id="${item.pid}" ${checked}/>
+             <div class="mdc-checkbox__background">
+               <svg class="mdc-checkbox__checkmark" viewBox="0 0 24 24">
+                 <path class="mdc-checkbox__checkmark-path" fill="none" stroke="white" d="M1.73,12.91 8.1,19.28 22.79,4.59" />
+               </svg>
+               <div class="mdc-checkbox__mixedmark"></div>
+             </div>
+           </div>
+         </span>
+         </li>
+         `);
+      $("#people").append(person);
+    });
+  }
+  return {
+    loading: function () {
+      $("#progressbar").show();
+      $("#datefield").hide();
+      $("#savebutton").hide();
+      $("#guest").hide();
+      var placeholder = `
+      <li class="mdc-list-item checkbox-list-ripple-surface placeholderRow">
+        <span class="mdc-list-item__text">
+            <div class="placeholderCell placeholderTypeA"></div>
+        </span>
+        <span class="mdc-list-item__text">
+            <div class="placeholderCell placeholderTypeB"></div>
+        </span>
+        <span class="mdc-list-item__meta">
+            <div class="placeholderCell placeholderTypeC"></div>
+        </span>
+      </li>`;
+      var list = placeholder.toString().repeat($(window).height() / 70);
+      $("#people").append(list);
+    },
+    show: function (data) {
+      draw(data);
       $("#progressbar").hide();
       $("#datefield").show();
       $("#savebutton").show();
       $("#guest").show();
       $("#people li.placeholderRow").remove();
     },
-    error: function (msg) {
-      alert(msg.responseText);
-    }
-  });
-}
-
-function drawData(data) {
-  // Check if data state already stored
-  var store = localStorage.getItem("chdir");
-  var members = [];
-  if (store) {
-    var storedata = JSON.parse(store);
-
-    // Only load stored data if from today
-    if (SelectedDate.today() == storedata.week) {
-      GuestCount.init(storedata.guests);
-      if (storedata.members) {
-        members = storedata.members.map(function (member) {
-          return member.id;
-        });
-      }
+    lock: function () {
+      $("#progressbar").show();
+      $("#top :input").attr("disabled", true);
+    },
+    unlock: function () {
+      $("#progressbar").hide();
+      $("#top :input").attr("disabled", false);
     }
   }
-
-  $.each(data, function (index, item) {
-    var checked = members.indexOf(item.pid.toString()) > -1 ? "checked" : "";
-    var person = $(`
-      <li class="mdc-list-item checkbox-list-ripple-surface">
-      <span class="mdc-list-item__graphic" role="presentation">
-        <i class="material-icons">how_to_reg</i>
-      </span>
-      <span class="mdc-list-item__text">
-        <label for="${item.pid}">${item.firstname} ${item.lastname}</label>
-      </span>
-      <span class="mdc-list-item__meta">
-        <div class="mdc-checkbox">
-          <input type="checkbox" class="mdc-checkbox__native-control" id="${item.pid}" ${checked}/>
-          <div class="mdc-checkbox__background">
-            <svg class="mdc-checkbox__checkmark" viewBox="0 0 24 24">
-              <path class="mdc-checkbox__checkmark-path" fill="none" stroke="white" d="M1.73,12.91 8.1,19.28 22.79,4.59" />
-            </svg>
-            <div class="mdc-checkbox__mixedmark"></div>
-          </div>
-        </div>
-      </span>
-      </li>
-      `);
-    $("#people").append(person);
-  });
-}
-
-function drawPlaceholder() {
-  $("#progressbar").show();
-  $("#datefield").hide();
-  $("#savebutton").hide();
-  $("#guest").hide();
-  var placeholder = `
-  <li class="mdc-list-item checkbox-list-ripple-surface placeholderRow">
-    <span class="mdc-list-item__text">
-        <div class="placeholderCell placeholderTypeA"></div>
-    </span>
-    <span class="mdc-list-item__text">
-        <div class="placeholderCell placeholderTypeB"></div>
-    </span>
-    <span class="mdc-list-item__meta">
-        <div class="placeholderCell placeholderTypeC"></div>
-    </span>
-  </li>`;
-  var list = placeholder.toString().repeat($(window).height() / 70);
-  $("#people").append(list);
-}
-
-// function saveData(data) {
-//   $("#progressbar").show();
-//   $("#top :input").attr("disabled", true);
-
-//   $.post({
-//     url: 'https://script.google.com/macros/s/URL/exec',
-//     type: "post",
-//     dataType: "json",
-//     data: data,
-//     success: function (data, textStatus, jqXHR) {
-//       $("#progressbar").hide();
-//       $("#top :input").attr("disabled", false);
-
-//       // Store to localstore if today
-//       if (SelectedDate.today() == SelectedDate.get()) {
-//         localStorage.setItem("chdir", JSON.stringify(data));
-//       }
-
-//       const result = { message: `Saved ${data.total} attendees for ${data.week}`, timeout: 5000 };
-//       _snackbar.show(result);
-//     },
-//     error: function (msg) {
-//       alert(msg.responseText);
-//     }
-//   });
-// }
+})();
 
 // Export functions to be accessible outside this bundle
 module.exports = {
+  GetSelected: GetSelected,
   GuestCount: GuestCount,
   SelectedDate: SelectedDate,
+  List: List,
   SnackBar: _snackbar
 };
